b88b43091ff91c3b1e642ed605353d03407130ae
Dan Wick <danwick@gmail.com>
2026-03-24 12:43:29 -0500

    Make Keystatic route handler resilient to missing credentials

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,7 +1,15 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler'
 import keystaticConfig from '../../../../keystatic.config'
 
-const handler = makeRouteHandler({ config: keystaticConfig })
+let handler: ReturnType<typeof makeRouteHandler>
+
+try {
+  handler = makeRouteHandler({ config: keystaticConfig })
+} catch {
+  // During build or when GitHub credentials are missing, provide a stub
+  const stub = () => new Response('Keystatic not configured', { status: 503 })
+  handler = { GET: stub, POST: stub }
+}
 
 export const POST = handler.POST
 export const GET = handler.GET
